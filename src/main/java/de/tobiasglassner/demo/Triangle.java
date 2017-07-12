1891f477b2c20a26933e87554e71c17c03297000
Tobias Glaßner <github@tobias-glassner.de>
2017-07-12 23:23:10 +0200

    Extended bean with constructor values. Set Triangle properties via constructor.

--- src/main/java/de/tobiasglassner/demo/Triangle.java
+++ src/main/java/de/tobiasglassner/demo/Triangle.java
@@ -2,8 +2,29 @@ package de.tobiasglassner.demo;
 
 public class Triangle {
 
+	private String type;
+	private int height;
+	
+	public Triangle(String type) {
+		this.type = type;
+	}
+
+	public Triangle(String type, int height) {
+		this.type = type;
+		this.height = height;
+	}
+	
+	
 	public void draw() {
-		System.out.println("Draws the triangle...");
+		System.out.println(getType() + " Triangle drawn of height " + getHeight());
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public int getHeight() {
+		return height;
 	}
 	
 }
